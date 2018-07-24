64a45fe1965915cba6313f65d2ed94ad38a14869
Dominic Miglar <dominic.miglar@netunix.at>
2018-07-24 21:45:09 +0200

    declare error and retry rabbitmq exchanges/queues/bindings too automatically

--- src/main/java/com/logreposit/influxdbservice/communication/messaging/rabbitmq/RabbitMqAutoConfigurationCommandLineRunner.java
+++ src/main/java/com/logreposit/influxdbservice/communication/messaging/rabbitmq/RabbitMqAutoConfigurationCommandLineRunner.java
@@ -1,13 +1,17 @@
 package com.logreposit.influxdbservice.communication.messaging.rabbitmq;
 
 import com.logreposit.influxdbservice.communication.messaging.common.MessageType;
+import com.logreposit.influxdbservice.communication.messaging.rabbitmq.error.retry.BackoffRetryStrategy;
 import com.logreposit.influxdbservice.configuration.RabbitConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Exchange;
 import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -40,6 +44,9 @@ public class RabbitMqAutoConfigurationCommandLineRunner implements CommandLineRu
 
     private void configureRabbit()
     {
+        this.declareErrorExchangeAndQueue();
+        this.declareRetryExchangesQueuesAndBindings();
+
         this.declareExchanges();
         this.declareBindings();
     }
@@ -48,15 +55,58 @@ public class RabbitMqAutoConfigurationCommandLineRunner implements CommandLineRu
     {
         for (MessageType messageType : MessageType.values())
         {
-            String   exchangeName = String.format("x.%s", messageType.toString().toLowerCase());
-            Exchange exchange     = ExchangeBuilder.fanoutExchange(exchangeName).durable(true).build();
+            String exchangeName = String.format("x.%s", messageType.toString().toLowerCase());
 
-            logger.warn("Declaring exchange '{}' for MessageType '{}' ...", exchangeName, messageType);
-
-            this.amqpAdmin.declareExchange(exchange);
-
-            logger.warn("Declared exchange '{}'.", exchangeName);
+            this.declareFanoutExchange(exchangeName);
         }
+    }
+
+    private void declareRetryExchangesQueuesAndBindings()
+    {
+        for (int retryInterval : BackoffRetryStrategy.RETRY_INTERVALS)
+        {
+            String   retryExchangeName = BackoffRetryStrategy.getExchangeNameForRetryInterval(retryInterval);
+            Exchange retryExchange     = this.declareFanoutExchange(retryExchangeName);
+
+            Queue retryQueue = QueueBuilder.durable("retry.q." + retryInterval)
+                                           .withArgument("x-dead-letter-exchange", "")
+                                           .withArgument("x-message-ttl", "10000")
+                                           .build();
+
+            this.amqpAdmin.declareQueue(retryQueue);
+
+            Binding binding = BindingBuilder.bind(retryQueue)
+                                            .to(retryExchange)
+                                            .with("")
+                                            .noargs();
+
+            this.amqpAdmin.declareBinding(binding);
+        }
+    }
+
+    private void declareErrorExchangeAndQueue()
+    {
+        Exchange errorExchange = ExchangeBuilder.directExchange("error.x").durable(true).build();
+
+        this.amqpAdmin.declareExchange(errorExchange);
+
+        Queue errorQueue = QueueBuilder.durable("error." + this.rabbitConfiguration.getQueue())
+                                       .build();
+
+        this.amqpAdmin.declareQueue(errorQueue);
+    }
+
+    private Exchange declareFanoutExchange(String exchangeName)
+    {
+        Exchange exchange = ExchangeBuilder.fanoutExchange(exchangeName).durable(true).build();
+
+        logger.warn("Declaring exchange '{}' ...", exchangeName);
+
+        this.amqpAdmin.declareExchange(exchange);
+
+        logger.warn("Declared exchange '{}'.", exchangeName);
+
+        return exchange;
     }
 
     private void declareBindings()
@@ -66,6 +116,7 @@ public class RabbitMqAutoConfigurationCommandLineRunner implements CommandLineRu
         for (MessageType messageType : SUBSCRIBED_MESSAGE_TYPES)
         {
             String  exchangeName = String.format("x.%s", messageType.toString().toLowerCase());
+
             Binding binding      = new Binding(queueName, Binding.DestinationType.QUEUE, exchangeName, "", new HashMap<>());
 
             logger.info("Declaring binding {} => {} ...", exchangeName, queueName);

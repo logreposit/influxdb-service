86092fc9f68b53beecc83d39d54fd7c4dc3639ba
Dominic Miglar <dominic.miglar@netunix.at>
2024-10-25 13:01:33 +0200

    spring boot & java 21 update

--- src/main/java/com/logreposit/influxdbservice/configuration/RabbitConfiguration.java
+++ src/main/java/com/logreposit/influxdbservice/configuration/RabbitConfiguration.java
@@ -1,6 +1,6 @@
 package com.logreposit.influxdbservice.configuration;
 
-import javax.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
